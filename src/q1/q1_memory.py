b9b53be8ac20c91c54c01630251ee8a4bb30a7b2
Vicente Lermanda <vlermanda@MacBook-Air-5.local>
2024-10-05 22:29:51 -0300

    Solution to q1 added

--- src/q1/q1_memory.py
+++ src/q1/q1_memory.py
@@ -1,5 +1,47 @@
 from typing import List, Tuple
 from datetime import datetime
+import json
+import logging
 
-def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
-    pass
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+
+def q1_memory(file_path: str) -> List[Tuple[int, datetime.date, str]]:
+    date_freq = {}
+    date_user_freq = {}
+
+    # Leer el archivo línea por línea
+    with open(file_path, 'r') as file:
+        for line in file:
+            entry = json.loads(line)
+            date_str = entry['date']
+            username = entry['user']['username']
+
+            # Convertir la fecha a un objeto datetime.date
+            date = datetime.fromisoformat(date_str).date()
+
+            # Actualizar contadores de fechas
+            if date not in date_freq:
+                date_freq[date] = 0
+                date_user_freq[date] = {}
+            date_freq[date] += 1
+
+            # Actualizar contadores de usuarios por fecha
+            if username not in date_user_freq[date]:
+                date_user_freq[date][username] = 0
+            date_user_freq[date][username] += 1
+
+    # Obtener las 10 fechas con más tweets
+    top_10_dates = sorted(date_freq.items(), key=lambda x: x[1], reverse=True)[:10]
+
+    result = []
+    for date, _ in top_10_dates:
+        # Obtener el usuario con más tweets en esa fecha y las frecuencias
+        top_user = max(date_user_freq[date].items(), key=lambda x: x[1])[0]
+        top_user_freq = date_user_freq[date][top_user]
+        # logger.info(f"""Date: {date}, Frequency: {date_freq[date]},
+        #             Top User: {top_user}, Top User Frequency: {top_user_freq}""")
+        result.append((date_freq[date], date, top_user))
+
+    return result
--- src/q1/q1_time.py
+++ src/q1/q1_time.py
@@ -1,5 +1,72 @@
 from typing import List, Tuple
 from datetime import datetime
+import json
+from heapq import heappush, heappop
+import logging
 
-def q1_time(file_path: str) -> List[Tuple[datetime.date, str]]:
-    pass
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+
+def q1_time(file_path: str) -> List[Tuple[int, datetime.date, str]]:
+    # Diccionarios para contar tweets por fecha y por usuario
+    date_freq = {}
+    date_user_freq = {}
+
+    # Heaps para mantener las fechas y los usuarios más frecuentes
+    date_user_heap = {}
+    date_heap = []
+
+    # Leer el archivo json línea por línea
+    with open(file_path, 'r') as file:
+        for line in file:
+            entry = json.loads(line)
+            date_str = entry['date']
+            username = entry['user']['username']
+
+            # Convertir la fecha a un objeto datetime.date
+            date = datetime.fromisoformat(date_str).date()
+
+            # Inicializar frecuencia y diccionarios si no existen
+            if date not in date_freq:
+                date_freq[date] = 0
+                date_user_freq[date] = {}
+                date_user_heap[date] = []
+
+            # Actualizar frecuencia de fecha y agregar a heap
+            date_freq[date] += 1
+            heappush(date_heap, (-1 * date_freq[date], date))
+
+            # Inicializar frecuencia de usuario si no existe
+            if username not in date_user_freq[date]:
+                date_user_freq[date][username] = 0
+
+            # Actualizar frecuencia de usuario para fecha especifica y agregar a heap
+            date_user_freq[date][username] += 1
+            heappush(date_user_heap[date], (-1 * date_user_freq[date][username], username))
+
+    # Lista de tuplas con las 10 fechas y usuarios más frecuentes a retornar
+    result = []
+
+    # Hacemos pop en el heap de fechas mientras no hayan 10 elementos en el resultado
+    while (len(date_heap) > 0) and (len(result) < 10):
+
+        # Sacamos la fecha con mayor frecuencia
+        date_top = heappop(date_heap)
+        date_top_freq = date_top[0] * -1
+        date_top_name = date_top[1]
+
+        # Verificamos si la fecha está repetida
+        if date_top_freq < date_freq[date_top_name]:
+            continue
+
+        # Obtenemos el usuario con mayor frecuencia para la fecha
+        user_top = heappop(date_user_heap[date_top_name])
+
+        # logger.info(f"""Date: {date_top_name}, Date freq: {date_top_freq},
+        #             User: {user_top[1]}, User freq: {user_top[0] * -1}""")
+
+        # Agregamos la fecha y usuario a la lista de resultados
+        result.append((date_top_freq, date_top_name, user_top[1]))
+
+    return result
